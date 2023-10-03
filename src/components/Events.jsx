d3b3749f9ed334d520bdb03046613995060d4a44
Aman Singh <yo.akaman@gmail.com>
2023-10-03 21:44:26 +0530

    'update'

--- src/components/Events.jsx
+++ src/components/Events.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import Nav from './Nav'
+import Footer from './Footer'
 
 const Events = () => {
   return (
     <>
     <Nav/>
     <div>Events</div>
+    <Footer/>
     </>
   )
 }
--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,14 +1,22 @@
 import React from "react";
 import { NavLink } from "react-router-dom";
 import logo from "./../assets/images/logo.webp";
-import { FaFacebook, FaGithub } from "react-icons/fa";
+import { FaFacebook, FaGithub, FaTwitter } from "react-icons/fa";
 import { AiFillYoutube } from "react-icons/ai";
 import { BsDiscord } from "react-icons/bs";
 
 const Footer = () => {
   return (
     <>
-      <div id="ft-section">
+
+      <div id="footer-section">
+
+        <div className="company-details">
+            <img className="nav_logo" src={logo} alt="logo" />
+            <div className="company-name">Pegasus Team</div>
+          </div>
+
+        <div className="newsLetter">
         <div className="ft-headline">Get News Letter</div>
         <div className="email-cont">
           <input type="text" name="email" placeholder="Your Email" />
@@ -16,27 +24,19 @@ const Footer = () => {
         <div className="btn-sec">
           <button className="btn">Subscribe</button>
         </div>
-
-        <div id="all">
-          <div className="company-details">
-            <img className="nav_logo" src={logo} alt="logo" />
-            <div className="company-name">Pegasus Team</div>
-          </div>
-          <div className="nav-icons">
-            <li><NavLink to="/">Home</NavLink>{" "}</li>
-            <li><NavLink to="/about">About</NavLink>{" "}</li>
-            <li><NavLink to="/events">Events</NavLink>{" "}</li>
-            <li><NavLink to="/contact">Contact</NavLink>{" "}</li>
-          </div>
-          <div className="social-icons">
-            <div className="fb-ico"><FaFacebook a href="https://google.com"/></div>
-            <div className="yt"><AiFillYoutube/></div>
-            <div className="github"><FaGithub/></div>
-            <div className="discord"><BsDiscord/></div>
-          </div>
-        </div>
         <div className="copyright-content">
           <p>Copyright © 2023 developed and maintained by #PegasusTeam</p>
+        </div>
+        </div>
+        
+        <div className="social-handle">
+        <div className="social-icons">
+            <li><NavLink to="https://fb.com" target="_blank" ><FaFacebook/></NavLink> </li>
+            <li><NavLink to="https://www.linkedin.com/" target="_blank" ><BsDiscord/></NavLink> </li>
+            <li><NavLink to="https://github.com" target="_blank" ><FaGithub/></NavLink> </li>
+            <li><NavLink to="https://youtube.com" target="_blank" ><AiFillYoutube/></NavLink> </li>
+            <li><NavLink to="https://twitter.com" target="_blank" ><FaTwitter/></NavLink> </li>
+          </div>
         </div>
       </div>
     </>
